1b535217e1edc91f909f1cf8310dd76adcfd1b25
riverhe360 <rhe@360insights.com>
2023-10-02 12:19:51 -0400

    create glue catalog database

---
+++ aws.tf
@@ -0,0 +1,20 @@
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch master
+#
+# Initial commit
+#
+# Changes to be committed:
+#	new file:   aws.tf
+#
+# Untracked files:
+#	.terraform.lock.hcl
+#	.terraform/
+#	aws.ft
+#	config
+#	credentials
+#	terraform.tfstate
+#	terraform.tfstate.backup
+#
